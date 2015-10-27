f7c7fc9624dc7f3af892f3bb00ef29e2218456e5
denis.solohub <dsolohub@grossum.com>
2015-10-27 14:21:24 +0200

    login fixed

--- src/main/resources/static/scripts/auth.service.js
+++ src/main/resources/static/scripts/auth.service.js
@@ -12,20 +12,37 @@ saApp.factory('AuthService', function ($http) {
         //        }
         //    );
         //},
+        //login: function(credentials) {
+        //    var data = 'username=' + encodeURIComponent(credentials.username) +
+        //        '&password=' + encodeURIComponent(credentials.password) +
+        //        '&submit=Login';
+        //    return $http.post('/login', data, {
+        //        headers: {
+        //            'Content-Type': 'application/x-www-form-urlencoded'
+        //        }
+        //    }).success(function (response) {
+        //        console.log('a');
+        //    }).catch(function(error) {
+        //        console.log('b');
+        //    });
+        //},
+
         login: function(credentials) {
-            var data = 'username=' + encodeURIComponent(credentials.username) +
-                '&password=' + encodeURIComponent(credentials.password) +
-                '&submit=Login';
-            return $http.post('/login', data, {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            }).success(function (response) {
-                console.log('a');
-            }).catch(function(error) {
-                console.log('b');
-            });
+           $http({
+               method: 'POST',
+               url: '/login',
+               data: credentials,
+               headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+           }).then(
+               function(){
+                   this.credentials = credentials;
+               },function(error){
+                   console.log(error);
+               }
+
+           );
         },
+
         register: function(user) {
             $http.post("/register", user);
         }
